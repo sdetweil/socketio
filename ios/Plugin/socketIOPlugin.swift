5eeed58ad87d6cf01376dd61d5650933753591b8
Sam Detweiler <sdetweil@gmail.com>
2021-08-04 23:14:32 -0500

    post run build

--- ios/Plugin/socketIOPlugin.swift
+++ ios/Plugin/socketIOPlugin.swift
@@ -1,6 +1,6 @@
 import Foundation
 import Capacitor
-
+import SocketIO
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
@@ -8,11 +8,21 @@ import Capacitor
 @objc(socketIOPlugin)
 public class socketIOPlugin: CAPPlugin {
     private let implementation = socketIO()
+    private var server = "http://localhost";
+    private var socket: SocketIOClient? = nil;
+    private var manager: SocketManager? = nil;
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    @objc func connect(_ call: CAPPluginCall) {
+      server=call.getString("server") ?? "sample";
+      manager = SocketManager(socketURL: URL(string:server)!);
+        socket=manager?.socket(forNamespace: "/");
+        socket?.connect();
+        call.resolve(["socket":socket!]);
+    }
+    @objc func emit(_ call: CAPPluginCall) {
+        let key = call.getString("key") ?? "key";
+        let data:Any = call.getObject("data") ?? "data";
+        socket!.emit(key, data as! [Any]);
+        call.resolve();
     }
 }
